b59eb450a4c621059b94924747b886449fd9552a
Piyush chaurasia <78043342+piyushchaurasia82@users.noreply.github.com>
2024-06-06 13:11:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Head>
         <title>Transform Images Effortlessly: online image converter</title>
         <meta name="description" content="Your Free Online Image Converter Convert any image format for free with NextConvert. Fast, easy, and reliable online tool to transform your images into the desired format in just a few clicks. No registration required. Try it now! NextConvert offers a seamless solution to transform your images hassle-free. With our free online image converter, unlock the power to switch formats effortlessly."/>
-    <link rel="canonical" href="https://nextconvert.in/">    
+    <link rel="canonical" href="https://nextconvert.in/"/>    
     <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="index,follow" />
         <meta name="googlebot" content="index,follow" />
