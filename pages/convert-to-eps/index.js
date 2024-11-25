37528fc7d261b9b3a7524f212568f0d819a6763a
Piyush chaurasia <78043342+piyushchaurasia82@users.noreply.github.com>
2024-11-25 12:00:50 +0530

    Update index.js

--- pages/convert-to-eps/index.js
+++ pages/convert-to-eps/index.js
@@ -43,8 +43,8 @@ const ConvertEps = () => {
                     </div>
                     <div className="col-span-2">
                         <div className="banner-text mb-4">
-                            <p className="text-center font-harmony-bold 2xl:text-5xl xl:text-4xl lg:text-4xl text-3xl">Convert an image to the EPS format</p>
-                            <p className=" text-center md:mt-6 mt-3 md:text-lg text-sm text-[#706f73]">Generate EPS files by converting your images, with optional digital filters available to enhance image quality.</p>
+                            <h1 className="text-center font-harmony-bold 2xl:text-5xl xl:text-4xl lg:text-4xl text-3xl">Convert an image to the EPS format</h1>
+                            <h2 className=" text-center md:mt-6 mt-3 md:text-lg text-sm text-[#706f73]">Generate EPS files by converting your images, with optional digital filters available to enhance image quality.</h2>
                         </div>
                         <img className="w-full" alt="Google-adsense" src="/dummy/ads1.png" />
                         <Uploader />
