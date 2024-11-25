192cf3d45ebcffbd0646e8876d2c15c732c9bd29
Piyush chaurasia <78043342+piyushchaurasia82@users.noreply.github.com>
2024-11-25 12:05:11 +0530

    Update index.js

--- pages/convert-to-tga/index.js
+++ pages/convert-to-tga/index.js
@@ -43,8 +43,8 @@ const ConvertTga = () => {
                     </div>
                     <div className="col-span-2">
                         <div className="banner-text mb-4">
-                            <p className="text-center font-harmony-bold 2xl:text-5xl xl:text-4xl lg:text-4xl text-3xl">Convert image to TGA format</p>
-                            <p className=" text-center md:mt-6 mt-3 md:text-lg text-sm text-[#706f73]">Our online converter supports converting all kinds of images to the TGA (Targa) format, complete with optional effect settings and digital filters.</p>
+                            <h1 className="text-center font-harmony-bold 2xl:text-5xl xl:text-4xl lg:text-4xl text-3xl">Convert image to TGA format</h1>
+                            <h2 className=" text-center md:mt-6 mt-3 md:text-lg text-sm text-[#706f73]">Our online converter supports converting all kinds of images to the TGA (Targa) format, complete with optional effect settings and digital filters.</h2>
                         </div>
                         <img className="w-full" alt="Google-adsense" src="/dummy/ads1.png" />
                         <Uploader />
