137add54242d171d4509f4eb05b30f6559d63cda
Piyush chaurasia <78043342+piyushchaurasia82@users.noreply.github.com>
2024-11-25 11:53:00 +0530

    Update stepthree.js

--- components/home/stepthree.js
+++ components/home/stepthree.js
@@ -5,22 +5,22 @@ const Stepthree = () => {
                 <div className="bg-[#f6f7f8] py-5 rounded-xl">
                     <div className="md:w-10/12 mx-auto text-center">
                         {/* <img className="w-6/12 mx-auto" alt="Google-adsense" src="/dummy/step1.svg"/> */}
-                        <p className=" font-harmony-bold text-xl">Convert Any File</p>
-                        <p className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">NextConvert supports more than 1500 file conversions. You can convert videos, images, audio files, or e-books. There are tons of Advanced Options to fine-tune your conversions.</p>
+                        <h2 className=" font-harmony-bold text-xl">Convert Any File</h2>
+                        <h3 className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">NextConvert supports more than 1500 file conversions. You can convert videos, images, audio files, or e-books. There are tons of Advanced Options to fine-tune your conversions.</h3>
                     </div>
                 </div>
                 <div className="bg-[#f6f7f8] py-5 rounded-xl">
                     <div className="md:w-10/12 mx-auto text-center">
                         {/* <img className="w-6/12 mx-auto" alt="Google-adsense" src="/dummy/step2.svg"/> */}
-                        <p className=" font-harmony-bold text-xl">Works Anywhere</p>
-                        <p className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">NextConvert is an online file converter. So it works on Windows, Mac, Linux, or any mobile device. All major browsers are supported. Simply upload a file and select a target format.</p>
+                        <h2 className=" font-harmony-bold text-xl">Works Anywhere</h2>
+                        <h3 className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">NextConvert is an online file converter. So it works on Windows, Mac, Linux, or any mobile device. All major browsers are supported. Simply upload a file and select a target format.</h3>
                     </div>
                 </div>
                 <div className="bg-[#f6f7f8] py-5 rounded-xl">
                     <div className="md:w-10/12 mx-auto text-center">
                         {/* <img className="w-6/12 mx-auto" alt="Google-adsense" src="/dummy/step3.svg"/> */}
-                        <p className=" font-harmony-bold text-xl">Privacy Guaranteed</p>
-                        <p className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">We know that file security and privacy are important to you. That is why we use 256-bit SSL encryption when transferring files and automatically delete them after a few hours.</p>
+                        <h2 className=" font-harmony-bold text-xl">Privacy Guaranteed</h2>
+                        <h3 className=" md:text-base text-sm text-[#5a5656] md:mt-5 mt-3">We know that file security and privacy are important to you. That is why we use 256-bit SSL encryption when transferring files and automatically delete them after a few hours.</h3>
                     </div>
                 </div>
             </div>
