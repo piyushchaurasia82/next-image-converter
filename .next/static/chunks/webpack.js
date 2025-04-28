79b768b751904cbfaf446470f026d60e0cf1cb91
piyushchaurasia82 <piyushchaurasia82@gmail.com>
2025-04-28 10:41:03 +0530

    uninstall

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83245283c69492c2"; }
+/******/ 		__webpack_require__.h = function() { return "4cb3e034712f8ab9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  appDir: true,
 }
 
 module.exports = nextConfig
